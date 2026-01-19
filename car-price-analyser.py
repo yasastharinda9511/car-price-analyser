0c1b81cebb8c6db1a0c5b00fed1f0f08d51ee278
yasastharinda9511 <yasastharinda9511@gmail.com>
2026-01-20 00:57:04 +0530

    searchin map added

--- car-price-analyser.py
+++ car-price-analyser.py
@@ -3,16 +3,28 @@ from extractor.RiyasewanaExtractor import RiyasewanaExtractor
 
 if __name__ == "__main__":
 
-    car_make_model = {
-        "toyota": ["yaris"]
+    vehicles = {
+        "cars": {
+            "toyota": ["vitz", "yaris", "passo"],
+            "suzuki": ["alto", "wagon-r"]
+        },
+        "suv": {
+            "toyota": ["yaris"]
+        }
     }
 
     riyasewanaExtractor = RiyasewanaExtractor()
 
-    for make in car_make_model :
-        for model in car_make_model[make]:
-            cars = riyasewanaExtractor.extract_data(vehicle_type= "cars", make= make, model= model)
-            print(f"Scraping complete. Total cars: {len(cars)}")
+    for vehicle_type, makes in vehicles.items():
+        for make, models in makes.items():
+            for model in models:
+                print(f"Scraping {vehicle_type}/{make}/{model}...")
+                cars = riyasewanaExtractor.extract_data(
+                    vehicle_type=vehicle_type,
+                    make=make,
+                    model=model
+                )
+                print(f"Scraping complete. Total: {len(cars)}")
 
-            # ikmanExtractor = IkmanExtractor()
-            # ikmanExtractor.extract_data(model="vitz")
+    # ikmanExtractor = IkmanExtractor()
+    # ikmanExtractor.extract_data(model="vitz")
